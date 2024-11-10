7cb57d0f46b2fc725d963f20a9eb96039287ae70
agus <agusidana04@gmail.com>
2024-11-10 16:17:20 -0300

    faltaba una llave

--- funciones.php
+++ funciones.php
@@ -8,16 +8,16 @@
 function consultaSQL($query){
   $local=false;
   if($local){
-    $link=mysqli_connect("localhost","root","","local") or die("Hubo un error al conectarse con la base de datos");
+    $link=mysqli_connect("localhost","root","","shopping") or die("Hubo un error al conectarse con la base de datos");
   }else{
     $link=mysqli_connect("server1661","u174220854_eg_shopping",'6qAGB$Hhtq&@ma+',"u174220854_shopping") or die("Hubo un error al conectarse con la base de datos");
-  
+  }
   $resultados = mysqli_query($link,$query) or die("Hubo un error con la transacción:".mysqli_error($link));
   mysqli_close($link);
   return $resultados;
 
 
-}}
+}
 
 
 
