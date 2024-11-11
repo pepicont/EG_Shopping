85d8f8d80bcf1cf9746641363e9c3d67fef86e8b
agus <agusidana04@gmail.com>
2024-11-11 11:13:45 -0300

    guardo

---
+++ noRegistrado/cards.php
@@ -0,0 +1,105 @@
+<?php
+// Logica de la paginación
+$limite = 6; // cantidad de resultados que se muestran en la página
+$pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1; // Página actual
+$principio = ($pagina - 1) * $limite; // el número del primer resultado que se mostrará en la página actual. Esto va a ir cambiando a medida que se avance de página
+
+//Lo que hace el Limit es que solo muestra los resultados que estan entre el principio y el limite
+//fin de lógica
+$totalpromociones= 0;
+
+$muestrafila = array();
+$muestrafila1 = array();
+$query = "SELECT * FROM locales WHERE estado = 'activo' $busqueda2";
+$resultado = consultaSQL($query);
+if (mysqli_num_rows($resultado) > 0) {
+    while ($fila = mysqli_fetch_array($resultado)) {
+        $query1 = "SELECT * FROM promociones WHERE  estadoPromo = 'activa' AND  codLocal =  '".$fila['codLocal']."' $busqueda ";
+        $resultado1 = consultaSQL($query1);
+        while ($fila1 = mysqli_fetch_array($resultado1)) {
+            $query2 = "SELECT * FROM uso_promociones WHERE codPromo = '" . $fila1["cod"] . "' AND estado = 'aceptada'";
+            $resultado2 = consultaSQL($query2);
+            if (mysqli_num_rows($resultado2) > 0) {
+                if ($estoy == 'verDescuentosUtilizados') {
+                    $encontro = 1;
+                    $muestrafila1[] = $fila1;
+                    $muestrafila[] = $fila;
+                    $totalpromociones++;
+                }
+            } else {
+                if ($estoy == 'verDescuentos') {
+                    $muestrafila1[] = $fila1;
+                    $muestrafila[] = $fila;
+                    $totalpromociones++;
+                    $encontro = 0;
+                }
+            }
+        }
+    }
+}
+
+$total_paginas = ceil($totalpromociones / $limite);
+$promociones_pagina = array_slice($muestrafila1 , $principio, $limite);
+$fila_pagina = array_slice($muestrafila, $principio, $limite);
+for ($i = 0; $i < count($promociones_pagina); $i++) {
+    mostrarcards($promociones_pagina[$i], $fila ,$pagina[$i], $encontro);
+}
+if ($total_paginas > 1) { ?> <!-- Muestra la paginación si hay más de una página -->
+    <div class="container w-100 d-flex justify-content-center">
+        <nav aria-label="..." class="mx-auto" style="width:fit-content">
+            <ul class="pagination justify-content-center" style="width:fit-content">
+                <li class="p <!-- Muestra la paginación si hay más de una página -->
+                    <a class="page-link" href="<?php if($pagina <= 1){ echo '#'; } else { $query = $_GET; $query['pagina'] = $pagina - 1; echo strtok($_SERVER['REQUEST_URI'], '?') . '?' . http_build_query($query); } ?>">Previous</a>
+                </li>
+                <?php
+                for ($i = 1; $i <= $total_pagnas; $i++) { 
+                    echo "<li class=\"page-item";
+                    if ($pagina == $i) echo " active";
+                    $query = $_GET; $query['pagina'] = $i;
+                    echo "\"><a class=\"page-link\" href='" . strtok($_SERVER['REQUEST_URI'], '?') . '?' . http_build_query($query) . "'>$i</a></li>";
+                }
+                ?>
+                <li class="page-item <?php if($pagina >= $total_paginas){ echo 'disabled'; } ?>">
+                <a class="page-link" href="<?php if($pagina >= $total_paginas){ echo '#'; } else { $query = $_GET; $query['pagina'] = $pagina + 1; echo strtok($_SERVER['REQUEST_URI'], '?') . '?' . http_build_query($query); } ?>">Next</a>
+                </li>
+            </ul>
+        </nav>
+</div>
+<?php } ?>
+
+<?php
+
+function mostrarcards($fila, $fila1, $encontro)
+{ ?>
+    <div class="card" style="margin: 15px; width: 18rem;">
+
+        <div class="card-body">
+            <div style="height:80%">
+                <h5 class="card-title">Cod descuento: <?php echo ($fila["cod"]) ?></h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Descripcion: <?php echo ($fila["textoPromo"]) ?></h6>
+                <p class="card-text">Del local: <?php echo ($fila1["nombreLocal"]) ?></p>
+                <p class="card-text">Rubro: <?php echo ($fila1["rubroLocal"]) ?></p>
+                <p class="card-text">Dias de la semana: <?php echo ($fila["diaSemana"]) ?></p>
+                <p class="card-text">Plazo: <?php echo ($fila["fechaDesde"]); echo (" --- "); echo ($fila["fechaHasta"]) ?></p>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Esta seguro que desea utilizar el descuento?</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-footer mx-auto text-center w-100 p-2 d-flex justify-content-between">
+                    <a href="utilizarDescuento.php?cod=<?php echo ($fila['cod']) ?>" class="btn btn-primary m-1">Utilizar descuento</a>
+                    <button type="button" class="btn btn-secundary m-1" data-bs-dismiss="modal">Cancelar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<?php
+/*header("Location: index.php");*/
+?>
---
+++ noRegistrado/verDescuentos.php
@@ -0,0 +1,56 @@
+<?php 
+    
+    include_once("../funciones.php");
+    include("../header.php");
+    $idusuario= $_SESSION["idUsuario"];
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Descuentos</title>
+    <link rel="stylesheet" href="../estilos/style1.css">
+</head>
+<body>
+    <div class="row">
+        <div class="col-12">
+            <?php 
+            $estoy="verDescuentos";
+            include("../breadCrumbs.php");
+            ?>
+        </div>
+    </div>
+    <div class="row">
+        <h1></h1>
+        <div class="col-3 filtros d-none d-lg-block"  style="height:fit-content;" >
+            <?php include("filtrosCli.php"); ?>
+        </div>
+        <div class="listado d-flex col-lg-8 col-12 mx-auto justify-content-center">
+        
+            <?php 
+                $estoy = 'verDescuentos';
+                include("cards.php"); ?>
+        </div>
+    </div>
+    <div class=" d-block d-lg-none"> 
+    <button class="btn btn-primary fixed-bottom end-0 m-3 bot-filtros" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" >FILTROS:</button>
+    <div class="offcanvas offcanvas-start filtros-desplegados" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel"></h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+                <?php 
+                    include("filtrosCLi.php"); 
+                ?>
+        </div>
+    </div>
+</div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<?php include('../footer.php') ?>
+</html>
